01fa2a82bf85553477eb0c0c601a0c0c8bd68614
rsobt <iida_t@eis.hokudai.ac.jp>
2021-11-26 16:47:26 +0900

    add iida.py

---
+++ samples/iida.py
@@ -0,0 +1,20 @@
+from qulacs import Observable, QuantumCircuit, QuantumState
+from qulacs.gate import Y, CNOT, merge
+
+
+def iida_sample():
+    state = QuantumState(3)
+    state.set_Haar_random_state()
+
+    circuit = QuantumCircuit(3)
+    circuit.add_Y_gate(0)
+    merged_gate = merge(CNOT(0,1),Y(1))
+    circuit.add_gate(merged_gate)
+    circuit.add_RY_gate(1,0.5)
+    circuit.update_quantum_state(state)
+
+    observable = Observable(3)
+    observable.add_operator(2.0, "X 2 Y 1 Z 0")
+    observable.add_operator(-3.0, "Z 2")
+    value = observable.get_expectation_value(state)
+    print(value)
